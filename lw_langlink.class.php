c37fa4c45d553ebba665b91a3467d600e8677710
Dr. Andreas Eckhoff <eckhoff@logicnetwork.de>
2012-04-28 12:34:08 +0200

    update: changes SQL to lw prepared statements

--- lw_langlink.class.php
+++ lw_langlink.class.php
@@ -23,21 +23,25 @@
 
 class lw_langlink extends lw_plugin {
 
-    function __construct() {
+    function __construct() 
+    {
         parent::__construct();
         $this->page = lw_page::getInstance($this->request->getIndex());
         $reg 	 	= lw_registry::getInstance();
     	$this->auth = $reg->getEntry("auth");
     }
     
-    function buildPageOutput() {
+    function buildPageOutput() 
+    {
         if ($this->params['develop_only'] == 1 && $this->auth->isLoggedIn()) {
-            $sql = "SELECT * FROM ".$this->db->gt('lw_page_langlink')." WHERE page_link > 0 AND page_id = ".$this->request->getIndex();
-            $erg = $this->db->select($sql);
+            $this->db->setStatement("SELECT * FROM t:lw_page_langlink WHERE page_link > 0 AND page_id = :id ");
+            $this->db->bindParameter('id', 'i', $this->request->getIndex());
+            $erg = $this->db->pselect();
 
             if (intval($this->params['template'])>0) {
-                $sql   = "SELECT * FROM ".$this->db->gt('lw_templates')." WHERE id = ".intval($this->params['template']);
-                $dummy = $this->db->select1($sql);
+                $this->db->setStatement("SELECT * FROM t:lw_templates WHERE id = :id ");
+                $this->db->bindParameter('id', 'i', $this->params['template']);
+                $dummy = $this->db->pselect1();
                 $tpla   = $dummy['template'];
             }
             
